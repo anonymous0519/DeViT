cd3063dac8bff66d53806414287066c299263b39
anonymous0519 <53685129+anonymous0519@users.noreply.github.com>
2022-06-17 18:05:42 +0800

    Delete segmentation directory

--- segmentation/configs/sem_fpn/DeViT/fpn_devit_b0_ade20k_40k.py
+++
@@ -1,9 +0,0 @@
-_base_ = './fpn_devit_b2_ade20k_40k.py'
-
-# model settings
-model = dict(
-    pretrained='pretrained/devit_b0.pth',
-    backbone=dict(
-        type='devit_b0',
-        style='pytorch'),
-    neck=dict(in_channels=[32, 64, 128, 256]))
--- segmentation/configs/sem_fpn/DeViT/fpn_devit_b1_ade20k_40k.py
+++
@@ -1,9 +0,0 @@
-_base_ = './fpn_devit_b2_ade20k_40k.py'
-
-# model settings
-model = dict(
-    pretrained='pretrained/devit_b1.pth',
-    backbone=dict(
-        type='devit_b1',
-        style='pytorch'),
-    neck=dict(in_channels=[64, 128, 256, 512]))
