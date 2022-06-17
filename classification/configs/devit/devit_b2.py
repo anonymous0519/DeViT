7091439ddeebe1fda206a980c576c39a20aad2d9
anonymous0519 <53685129+anonymous0519@users.noreply.github.com>
2022-06-17 18:05:16 +0800

    Delete classification directory

--- classification/configs/devit/devit_b2.py
+++
@@ -1,6 +0,0 @@
-cfg = dict(
-    model='devit_b2',
-    drop_path=0.1,
-    clip_grad=None,
-    output_dir='checkpoints/devit_b2',
-)
--- classification/configs/devit/devit_b5.py
+++
@@ -1,6 +0,0 @@
-cfg = dict(
-    model='devit_b5',
-    drop_path=0.3,
-    clip_grad=1.0,
-    output_dir='checkpoints/devit_b5',
-)
